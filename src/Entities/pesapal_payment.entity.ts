624d233f7f10536380ba74394a5674344c271d05
sam-zarila <zarilatroxx99@gmail.com>
2025-01-27 05:40:15 -0800

    for payments

--- src/Entities/pesapal_payment.entity.ts
+++ src/Entities/pesapal_payment.entity.ts
@@ -1,4 +1,27 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 
 @Entity()
+
+export class paymentEntity{
+
+    @PrimaryGeneratedColumn()
+     id:number
+
+     @Column()
+     amount: number; // Payment amount
+
+        @Column()
+        currency: string; // Currency (e.g., ZMW)
+
+        @Column()
+        status: string; // Payment status (e.g., PENDING, COMPLETED, FAILED)
+
+        @Column()
+        paymentMethod: string; // Payment method (e.g., MOBILE_MONEY, CARD)
+
+        @Column()
+        createdAt: Date;
+
+
+}
