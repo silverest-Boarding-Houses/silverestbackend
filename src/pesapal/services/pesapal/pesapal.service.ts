c10d8088015e550a20cbef7d7732d1f6ddbf2d54
sam-zarila <zarilatroxx99@gmail.com>
2025-01-27 15:14:13 -0800

    for payments

--- src/pesapal/services/pesapal/pesapal.service.ts
+++ src/pesapal/services/pesapal/pesapal.service.ts
@@ -1,4 +1,15 @@
+import { config } from 'process';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PesapalService {}
+export class PesapalService {
+
+    private readonly pesapalUrl: string;
+    private readonly consumerKey: string;
+    private readonly consumerSecret: string;
+
+    constructor(private configService:ConfigService){
+        this.consumerKey = this.configService.get<string>('PESAPAL_CONSUMER_KEY')
+    }
+}
