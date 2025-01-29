a0f25c835bdb550208abe72b3ace478ada897003
sam-zarila <zarilatroxx99@gmail.com>
2025-01-29 15:13:42 -0800

    for payments

--- src/pawapay/DTO/pawapayDTO.ts
+++ src/pawapay/DTO/pawapayDTO.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, IsIn } from 'class-validator';
 
 export class CreatePaymentDto {
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   amount: number;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   @IsIn(['zmw']) // Example currencies
   currency: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   customerPhone: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   transactionId: string;
 }
