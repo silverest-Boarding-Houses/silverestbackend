246e95af404ab1101461973fac1dfa84a312f450
sam-zarila <zarilatroxx99@gmail.com>
2025-01-29 15:30:02 -0800

    for payments

--- src/pawapay/pawapay.module.ts
+++ src/pawapay/pawapay.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PawapayController } from './controllers/pawapay/pawapay.controller';
 import { PawapayService } from './service/pawapay/pawapay.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PawaPaymentEnity } from 'src/Entities/pawapay.Entity';
+import { HttpService } from '@nestjs/axios';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([])],
+  imports:[TypeOrmModule.forFeature([PawaPaymentEnity]),HttpService],
   controllers: [PawapayController],
   providers: [PawapayService]
 })
--- src/pawapay/service/pawapay/pawapay.service.ts
+++ src/pawapay/service/pawapay/pawapay.service.ts
@@ -1,4 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PawaPaymentEnity } from 'src/Entities/pawapay.Entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PawapayService {}
+export class PawapayService {
+    private readonly apiKey: string;
+    private readonly baseUrl: string;
+
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly ConfigService: ConfigService,
+
+        @InjectRepository(PawaPaymentEnity) private  readonly pawapayEntity:Repository<PawaPaymentEnity>,
+    ){
+
+    }
+}
